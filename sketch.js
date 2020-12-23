9ac7e5b43db9b08daa93833a40ee5160ef4ded1a
kittykat6 <75424928+kittykat6@users.noreply.github.com>
2020-12-23 16:54:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var movingrect ,fixedrect;
+function setup() {
+  createCanvas(800,400);
+ fixedrect = createSprite(400, 200, 50, 50);
+ movingrect = createSprite(10, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255); 
+  movingrect.x = mouseX
+  movingrect.y = mouseY
+  if(movingrect.x - fixedrect.x<movingrect.width/2+fixedrect.width/2 &&
+    fixedrect.x - movingrect.x<fixedrect.width/2+movingrect.width/2 &&
+    movingrect.y - fixedrect.y<movingrect.height/2+fixedrect.height/2 &&
+    fixedrect.y - movingrect.y<fixedrect.height/2+movingrect.height/2 
+    ) {
+      movingrect.shapeColor = "yellow"
+      fixedrect.shapeColor = "pink"
+    }
+    else{
+      fixedrect.shapeColor = "yellow"
+      movingrect.shapeColor = "yellow"
+    }
+  drawSprites();
+}
